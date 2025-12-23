eb55f3fa76769af914e621ae7a473b1bcb60e21f
DannyTuanAnh <trantuananh070405@gmail.com>
2025-12-24 00:03:59 +0700

    update movie model and store with more info about film

--- backend/internal/model/movie.go
+++ backend/internal/model/movie.go
@@ -1,8 +1,16 @@
 package model
 
+import "time"
+
 type Movie struct {
-	MovieID     int    `json:"movie_id" db:"movie_id"`
-	Title       string `json:"title" db:"title"`
-	Duration    int    `json:"duration" db:"duration"`
-	Description string `json:"description" db:"description"`
+	MovieID     int       `json:"movie_id" db:"movie_id"`
+	Title       string    `json:"title" db:"title"`
+	Duration    int       `json:"duration" db:"duration"`
+	Description string    `json:"description" db:"description"`
+	Url         string    `json:"url_image" db:"url_image"`
+	Rate        float32   `json:"rate" db:"rate"`
+	Genre       string    `json:"genre" db:"genre"`
+	ReleaseDate time.Time `json:"release_date" db:"release_date"`
+	Director    string    `json:"director" db:"director"`
+	Cast        string    `json:"cast" db:"cast_list"`
 }
--- backend/internal/store/movie_store.go
+++ backend/internal/store/movie_store.go
@@ -24,14 +24,14 @@ func NewMovieStore(db *sql.DB) MovieStore {
 }
 
 func (s *movieStore) GetAll() ([]model.Movie, error) {
-	query := `SELECT movie_id, title, duration, description FROM movies ORDER BY title`
-	
+	query := `SELECT * FROM movies ORDER BY title`
+
 	rows, err := s.db.Query(query)
 	if err != nil {
 		return nil, fmt.Errorf("failed to query movies: %w", err)
 	}
 	defer rows.Close()
-	
+
 	var movies []model.Movie
 	for rows.Next() {
 		var movie model.Movie
@@ -40,34 +40,46 @@ func (s *movieStore) GetAll() ([]model.Movie, error) {
 			&movie.Title,
 			&movie.Duration,
 			&movie.Description,
+			&movie.Url,
+			&movie.Rate,
+			&movie.Genre,
+			&movie.ReleaseDate,
+			&movie.Director,
+			&movie.Cast,
 		)
 		if err != nil {
 			return nil, fmt.Errorf("failed to scan movie: %w", err)
 		}
 		movies = append(movies, movie)
 	}
-	
+
 	return movies, nil
 }
 
 func (s *movieStore) GetByID(movieID int) (*model.Movie, error) {
-	query := `SELECT movie_id, title, duration, description FROM movies WHERE movie_id = $1`
-	
+	query := `SELECT * FROM movies WHERE movie_id = $1`
+
 	var movie model.Movie
 	err := s.db.QueryRow(query, movieID).Scan(
 		&movie.MovieID,
 		&movie.Title,
 		&movie.Duration,
 		&movie.Description,
+		&movie.Url,
+		&movie.Rate,
+		&movie.Genre,
+		&movie.ReleaseDate,
+		&movie.Director,
+		&movie.Cast,
 	)
-	
+
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, fmt.Errorf("movie not found")
 		}
 		return nil, fmt.Errorf("failed to get movie: %w", err)
 	}
-	
+
 	return &movie, nil
 }
 
@@ -78,13 +90,13 @@ func (s *movieStore) GetShows(movieID int) ([]model.ShowWithMovie, error) {
 		JOIN movies m ON s.movie_id = m.movie_id
 		WHERE s.movie_id = $1
 		ORDER BY s.show_time`
-	
+
 	rows, err := s.db.Query(query, movieID)
 	if err != nil {
 		return nil, fmt.Errorf("failed to query shows: %w", err)
 	}
 	defer rows.Close()
-	
+
 	var shows []model.ShowWithMovie
 	for rows.Next() {
 		var show model.ShowWithMovie
@@ -99,7 +111,7 @@ func (s *movieStore) GetShows(movieID int) ([]model.ShowWithMovie, error) {
 		}
 		shows = append(shows, show)
 	}
-	
+
 	return shows, nil
 }
 
@@ -109,13 +121,13 @@ func (s *movieStore) GetAllShows() ([]model.ShowWithMovie, error) {
 		FROM shows s
 		JOIN movies m ON s.movie_id = m.movie_id
 		ORDER BY s.show_time`
-	
+
 	rows, err := s.db.Query(query)
 	if err != nil {
 		return nil, fmt.Errorf("failed to query shows: %w", err)
 	}
 	defer rows.Close()
-	
+
 	var shows []model.ShowWithMovie
 	for rows.Next() {
 		var show model.ShowWithMovie
@@ -130,30 +142,36 @@ func (s *movieStore) GetAllShows() ([]model.ShowWithMovie, error) {
 		}
 		shows = append(shows, show)
 	}
-	
+
 	return shows, nil
 }
 
 // GetByShowID retrieves movie information by show ID
 func (s *movieStore) GetByShowID(showID int) (*model.Movie, error) {
-    query := `
-        SELECT m.movie_id, m.tittle, m.duration, m.description
+	query := `
+        SELECT m.*
         FROM movies m
         INNER JOIN shows s ON m.movie_id = s.movie_id
         WHERE s.show_id = $1
     `
-    
-    var movie model.Movie
-    err := s.db.QueryRow(query, showID).Scan(
-        &movie.MovieID,
-        &movie.Title,
-        &movie.Duration,
-        &movie.Description,
-    )
-    
-    if err != nil {
-        return nil, fmt.Errorf("movie not found for show %d: %w", showID, err)
-    }
-    
-    return &movie, nil
+
+	var movie model.Movie
+	err := s.db.QueryRow(query, showID).Scan(
+		&movie.MovieID,
+		&movie.Title,
+		&movie.Duration,
+		&movie.Description,
+		&movie.Url,
+		&movie.Rate,
+		&movie.Genre,
+		&movie.ReleaseDate,
+		&movie.Director,
+		&movie.Cast,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("movie not found for show %d: %w", showID, err)
+	}
+
+	return &movie, nil
 }
